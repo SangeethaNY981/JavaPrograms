5753819989674b15d4c16b72b927446602a57637
SangeethaNY981 <147474144+SangeethaNY981@users.noreply.github.com>
2023-10-14 11:30:13 +0530

    Add files via upload

---
+++ Months.java
@@ -0,0 +1,29 @@
+package Examples;
+
+import BasicPrograms.Day;
+
+public class Months {
+	
+	 enum months{
+		jan ,
+		feb,
+		march,
+		april,
+		may,
+		june,
+		july,
+		august,
+		september,
+		oct,
+		nov,
+		dec;
+	}
+
+	
+	public static void main(String[] args) {
+	
+		  months Year = months. april;
+		System.out.println("Month is " +Year);
+	}
+
+}
