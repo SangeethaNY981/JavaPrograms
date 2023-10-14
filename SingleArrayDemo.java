27cb6b00b0ec7fc056072264bb0bd58b4446fa29
SangeethaNY981 <147474144+SangeethaNY981@users.noreply.github.com>
2023-10-14 11:20:58 +0530

    Add files via upload

---
+++ SingleArrayDemo.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class SingleArrayDemo {
+
+	public static void main(String[] args) {
+		int x[]= {1,2,3,4,5};
+		System.out.println(x[0]);
+		System.out.println(x[1]);
+		System.out.println(x[2]);
+		System.out.println(x[3]);
+		System.out.println(x[4]);
+		
+		System.out.println();
+		
+		System.out.println("this is for String type");
+		
+		String[] s= {"a","aa","aaa"};
+		System.out.println("length of the array"+s.length);
+	
+
+	}
+
+}
