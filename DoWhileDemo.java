6334fd42e91ad4d5b22feec746ccda91dc378a11
SangeethaNY981 <147474144+SangeethaNY981@users.noreply.github.com>
2023-10-14 13:01:14 +0530

    Add files via upload

---
+++ DoWhileDemo.java
@@ -0,0 +1,42 @@
+package IterativeStatements;
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		
+	
+		//1
+//		do
+//			System.out.println("1");
+//		while (true);
+		
+		//2
+//		do 
+//			int x=10;     //it wont allow declaration without {} braces
+//		 while(true)
+//			 System.out.println("1");
+		
+		//3
+//	do  {
+//			System.out.println("hi");
+//		 System.out.println("Hello");
+//		}
+//	  while(true) ;
+		
+		//4
+//		do {
+//   			int x=10;
+//			System.out.println("hello");
+//		}
+//		while(false);
+		
+		//5
+		  do 
+	    System.out.println("hi");
+		while(true)
+	   System.out.println("hello");
+	   while(false);
+	
+	}
+
+}
