11a14c5554c79576972398b2a2171dca7003a864
SangeethaNY981 <147474144+SangeethaNY981@users.noreply.github.com>
2023-10-28 12:42:01 +0530

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,17 +1,21 @@
-package WhileCondition;
+package basicProgramsExamples;
+
+import java.util.Scanner;
 
 public class Factorial {
 
 	public static void main(String[] args) {
-		int num=6;
-		int fact=1;
-		int i=1;
-		while(i<=num) {
-			fact = fact*i;
-			i++;
+		
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the Number :");
+		int num =sc.nextInt();
+		int fact =1;
+		for(int i=1;i<=num;i++) {
+			fact *=i;
 			System.out.println(fact);
 		}
-  System.out.println(fact);
+		System.out.println(fact);
+
 	}
 
 }
